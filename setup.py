c8a8c6946b1c86ca68f60cee75644052696231b8
nisha <nishanth14kpm@gmail.com>
2024-06-24 09:47:55 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HMS',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='nishanth',
+    author_email='nishanths.23aim@komgu,edu',
+    description='HospitalManagementSystem for college python course'
+)
